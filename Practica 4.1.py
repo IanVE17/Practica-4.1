436d89e86e0c1139894c39261d8158e8c9093857
Ian Velazquez <ianvelazquez117@gmail.com>
2022-09-21 23:11:01 -0500

    First commit

---
+++ Practica 4.1.py
@@ -0,0 +1,20 @@
+# Importes 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+# Seleccionar el WebDriver y definir la URL
+d = webdriver.Chrome(executable_path=r"C:\Users\ianve\Documents\Universidad\7 Semestre\Calidad de Software\chromedriver.exe")
+d.get("http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html")
+d.maximize_window()
+
+sources = ["box1", "box2", "box3", "box4", "box5", "box6", "box7"]
+targets = ["box101", "box102", "box103", "box104", "box105", "box106", "box107"]
+# print(source[2])
+
+for i in range(0, 7):
+    source = d.find_element(By.ID, sources[i])
+    target = d.find_element(By.ID, targets[i])
+    actions = ActionChains(d)
+    actions.drag_and_drop(source, target)
+    actions.perform()
